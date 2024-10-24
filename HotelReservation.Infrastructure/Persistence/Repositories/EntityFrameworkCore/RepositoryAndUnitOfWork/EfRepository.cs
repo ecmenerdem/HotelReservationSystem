4f1055ec2c0683a7a7c697477a3d554f8aa13a41
ecmenerdem <ecmenerdem@gmail.com>
2024-10-24 15:38:41 +0300

    EF Repositoryler oluşturuldu

---
+++ HotelReservation.Infrastructure/Persistence/Repositories/EntityFrameworkCore/RepositoryAndUnitOfWork/EfRepository.cs
@@ -0,0 +1,94 @@
+﻿using HotelReservation.Domain.Entities.Base;
+using HotelReservation.Domain.Repositories.DataManagement;
+using HotelReservation.Infrastructure.Persistence.EFCore.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelReservation.Infrastructure.Persistence.Repositories.EntityFrameworkCore.RepositoryAndUnitOfWork
+{
+    public class EfRepository<T> : IRepository<T> where T : AuditableEntity
+    {
+        protected readonly DbContext _context;
+        private readonly DbSet<T> _dbSet;
+
+        public EfRepository(DbContext context)
+        {
+            _context = context;
+            _dbSet = _context.Set<T>();
+        }
+
+        public async Task<T> GetAsync(Expression<Func<T, bool>> filter, params string[] includeProperties)
+        {
+            IQueryable<T> query = _dbSet;
+
+            // Include edilen navigation property'leri ekle
+            foreach (var includeProperty in includeProperties)
+            {
+                query = query.Include(includeProperty);
+            }
+
+            return await query.SingleOrDefaultAsync(filter);
+        }
+
+        public async Task<T> GetByIdAsync(int id)
+        {
+            return await _dbSet.FindAsync(id);
+        }
+
+        public async Task<IQueryable<T>> GetAllAsync(Expression<Func<T, bool>> filter = null, params string[] includeProperties)
+        {
+            IQueryable<T> query = _dbSet;
+
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+
+            // Include edilen navigation property'leri ekle
+            foreach (var includeProperty in includeProperties)
+            {
+                query = query.Include(includeProperty);
+            }
+
+            return await Task.FromResult(query);
+        }
+
+        public async Task<IQueryable<T>> GetAllAsyncAsNoTracking(Expression<Func<T, bool>> filter = null, params string[] includeProperties)
+        {
+            IQueryable<T> query = _dbSet.AsNoTracking();
+
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+
+            // Include edilen navigation property'leri ekle
+            foreach (var includeProperty in includeProperties)
+            {
+                query = query.Include(includeProperty);
+            }
+
+            return await Task.FromResult(query);
+        }
+
+        public async ValueTask AddAsync(T entity)
+        {
+            await _dbSet.AddAsync(entity);
+        }
+
+        public void Update(T entity)
+        {
+            _dbSet.Update(entity);
+        }
+
+        public void Remove(T entity)
+        {
+            _dbSet.Remove(entity);
+        }
+    }
+}
