0acc4ec9b634589ec5c99251cce773efcad35ea2
ecmen <ecmen@ECMENERDEM>
2024-10-29 01:57:46 +0300

    .

---
+++ HotelReservation.WebAPI/Controllers/UserController.cs
@@ -0,0 +1,23 @@
+﻿using HotelReservation.Application.Contracts.Persistence;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelReservation.WebAPI.Controllers;
+
+[Route("HotelReservationApi/[action]")]
+[ApiController]
+public class UserController : Controller
+{
+    private readonly IUserService _userService;
+
+    public UserController(IUserService userService)
+    {
+        _userService = userService;
+    }
+
+    [HttpGet("/Users")]
+    public async Task<IActionResult> GetAllUsers()
+    {
+        var users = await _userService.GetAllUsersAsync();
+        return Ok(users);
+    }
+}
