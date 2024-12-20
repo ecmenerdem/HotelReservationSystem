6c6ba76ed9c9c4a4bbe2797a48fd8d1bc4303bc5
ecmen <ecmen@ECMENERDEM>
2024-12-20 23:13:08 +0300

    .

---
+++ HotelReservation.WebAPI/Controllers/HotelController.cs
@@ -0,0 +1,60 @@
+﻿using AutoMapper;
+using HotelReservation.Application.Contracts.Persistence;
+using HotelReservation.Application.DTO.Hotel;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelReservation.WebAPI.Controllers;
+
+[ApiController]
+[Route("HotelReservationApi/[action]")]
+public class HotelController : Controller
+{
+    private readonly IHotelService _hotelService;
+    private readonly IMapper _mapper;
+
+    public HotelController(IHotelService hotelService, IMapper mapper)
+    {
+        _hotelService = hotelService;
+        _mapper = mapper;
+    }
+
+    // Get Hotel by ID
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetHotelById(int id)
+    {
+        var result = await _hotelService.GetHotelByIdAsync(id);
+        return StatusCode((int)result.StatusCode, result.Error);
+    }
+
+    // Get All Hotels
+    [HttpGet]
+    public async Task<IActionResult> GetAllHotels()
+    {
+        var result = await _hotelService.GetAllHotelsAsync();
+        return StatusCode((int)result.StatusCode, result.Error);
+    }
+
+    // Add a New Hotel
+    [HttpPost]
+    public async Task<IActionResult> AddHotel([FromBody] HotelAddRequestDTO hotelAddRequestDto)
+    {
+        var result = await _hotelService.AddHotelAsync(hotelAddRequestDto);
+        return StatusCode((int)result.StatusCode, result.Error);
+    }
+
+    // Update Hotel
+    [HttpPut("{id}")]
+    public async Task<IActionResult> UpdateHotel([FromBody] HotelUpdateRequestDTO hotelUpdateRequestDto)
+    {
+        var result = await _hotelService.UpdateHotelAsync(hotelUpdateRequestDto);
+        return StatusCode((int)result.StatusCode, result.Error);
+    }
+
+    // Delete Hotel
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteHotel(int id)
+    {
+        var result = await _hotelService.DeleteHotelAsync(id);
+        return StatusCode((int)result.StatusCode, result.Error);
+    }
+}
