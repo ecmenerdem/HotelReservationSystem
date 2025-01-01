3da6f111875cb05b9375f581a95d2c1883eca1a5
ecmen <ecmen@ECMENERDEM>
2025-01-01 15:08:36 +0300

    .

---
+++ HotelReservation.WebHelper/APIHelper/Result/ApiResult.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace HotelReservation.WebHelper.APIHelper.Result
+{
+    public class ApiResult<T>
+    {
+        public bool Success { get; set; }
+        public string Message { get; set; }
+        public T Data { get; set; }
+
+        public ErrorResult Error { get; set; }
+
+
+        [JsonIgnore] public HttpStatusCode StatusCode { get; set; }
+
+    }
+}
---
+++ HotelReservation.WebHelper/APIHelper/Result/ErrorResult.cs
@@ -0,0 +1,6 @@
+﻿namespace HotelReservation.WebHelper.APIHelper.Result;
+
+public class ErrorResult
+{
+    public List<string> Errors { get; set; }
+}
